28cb21a010d5f88b87c6bc49ff0d321e62d9fa05
Lucas Hames <lucas@hames.dev>
2025-02-14 19:05:02 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TibiaLabs API')
+    .setDescription(
+      'Useful informations for your Tibian applications or stream.',
+    )
+    .setVersion('3.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
